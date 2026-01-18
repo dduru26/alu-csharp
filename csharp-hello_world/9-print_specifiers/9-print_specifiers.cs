afe8871f223989a272bcb7baa35ee5db0db2b92e
dduru26 <d.duru@alustudent.com>
2026-01-18 08:15:55 +0200

    10th code in my first assignment

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("Percent: {0:F2}% tip", percent * 100);
+		Console.WriteLine("Currency: {0:C}", currency);
+	}
+}
